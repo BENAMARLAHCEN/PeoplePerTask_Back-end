aaebca848cf2d0fe96f8451a20f785befb5d7be7
lahcen <lahcen.ben3@gmail.com>
2023-11-25 23:32:52 +0100

    sidebar denamic select page link

--- project/dashboard/include/aside.php
+++ project/dashboard/include/aside.php
@@ -1,34 +1,37 @@
 <aside id="sidebar" class="side">
             <div class="h-100">
                 <div class="sidebar_logo d-flex align-items-end">
-                    <img src="img/logo.svg" alt="">
-                    <a href="#" class="nav-link text-white-50">Dashboard</a>
-                    <img class="close align-self-start" src="img/close.svg" alt="">
+                    <img src="<?=$place?>img/logo.svg" alt="">
+                    <a href="<?=$place?>dashboard.php" class="nav-link text-white-50">Dashboard</a>
+                    <img class="close align-self-start" src="<?=$place?>img/close.svg" alt="">
                 </div>
 
                 <ul class="sidebar_nav">
                     <li class="sidebar_item <?=$active_overview ?>" style="width: 100%;">
-                        <a href="dashboard.php" class="sidebar_link"> <img src="img/1. overview.svg" alt="">Overview</a>
+                        <a href="<?=$place?>dashboard.php" class="sidebar_link"> <img src="<?=$place?>img/1. overview.svg" alt="">Overview</a>
                     </li>
                     <li class="sidebar_item <?=$active_users ?>">
-                        <a href="users.php" class="sidebar_link"> <img src="img/agents.svg" alt="">Users</a>
+                        <a href="<?=$place?>users.php" class="sidebar_link"> <img src="<?=$place?>img/agent.svg" alt="">Users</a>
                     </li>
                     <li class="sidebar_item <?=$active_freelances ?>">
-                        <a href="freelances.php" class="sidebar_link"> <img src="img/agents.svg" alt="">Freelances</a>
+                        <a href="<?=$place?>freelances.php" class="sidebar_link"> <img src="<?=$place?>img/agents.svg" alt="">Freelances</a>
+                    </li>
+                    <li class="sidebar_item <?=$active_testimonials?>">
+                        <a href="<?=$place?>testimonials.php" class="sidebar_link"><img src="<?=$place?>img/articles.svg" alt="">testimonials</a>
                     </li>
                     <li class="sidebar_item<?=$active_project ?>">
-                        <a href="projects.php" class="sidebar_link"> <img src="img/task.svg" alt="">projects</a>
+                        <a href="<?=$place?>projects.php" class="sidebar_link"> <img src="<?=$place?>img/task.svg" alt="">projects</a>
                     </li>
                     <li class="sidebar_item<?=$active_contact ?>">
-                        <a href="contact.php" class="sidebar_link"><img src="img/agent.svg" alt="">Contact</a>
+                        <a href="<?=$place?>contact.php" class="sidebar_link"><img src="<?=$place?>img/agent.svg" alt="">Contact</a>
                     </li>
                     <li class="sidebar_item <?=$active_categorie?>">
-                        <a href="#" class="sidebar_link"><img src="img/articles.svg" alt="">Articles</a>
+                        <a href="<?=$place?>categories.php" class="sidebar_link"><img src="<?=$place?>img/articles.svg" alt="">Categories</a>
                     </li>
 
                 </ul>
                 <div class="line"></div>
-                <a href="#" class="sidebar_link"><img src="img/settings.svg" alt="">Settings</a>
+                <a href="<?=$place?>settings.php" class="sidebar_link"><img src="<?=$place?>img/settings.svg" alt="">Settings</a>
 
 
             </div>
