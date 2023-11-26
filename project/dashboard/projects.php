1b96e0b70c37eb858f837f995a2ea263e931cb43
lahcen <lahcen.ben3@gmail.com>
2023-11-26 16:20:49 +0100

    Created UI components to support project management

--- project/dashboard/projects.php
+++ project/dashboard/projects.php
@@ -4,27 +4,22 @@
 $active_overview = '';
 $active_users = '';
 $active_freelances = '';
+$active_testimonials = '';
 $active_project = 'active';
 $active_contact = '';
 $active_categorie = '';
+$place = '';
 ?>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="dashboard.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-</head>
+<?php
+include('include/head.php');
+?>
 
 <body>
     <div class="wrapper">
         <?php include('include/aside.php') ?>
         <div class="main">
         <?php include('include/navbar.php') ?>
-        <a class='btn btn-primary' style="width : 10rem;" href='adduser.php'>ADD FREELANCE</a>
+        <a class='btn btn-primary' style="width : 10rem;" href='create/creatproject.php'>ADD PROJECT</a>
             <div class="Agents">
             <table id="example" class="table table-striped" style="width:100%">
                 <thead>
@@ -40,30 +35,28 @@ $active_categorie = '';
                 <tbody>
 
                 <?php
-               
-
-
-                   $sql = "SELECT * FROM projects 
+                    include('include/connect.php');
+                   $sql = "SELECT projects.id as id_pro,title,project_description,CategoryName,subName FROM projects 
                    LEFT JOIN sub_categories on sub_categories.id = projects.id_sub_category
                    LEFT JOIN categories on categories.id = sub_categories.id_category
                    ";
                    $user = mysqli_query($con,$sql);
 
                    if (!$user) {
-                    die("invaled query: " . mysqli_error());
+                    die("invaled query: " . mysqli_error($con));
                   }
 
                   while ($row = mysqli_fetch_assoc($user)){
                     echo "
                     <tr>
-                        <td>$row[id_sub_category]</td>
+                        <td>$row[id_pro]</td>
                         <td>$row[title]</td>
                         <td>$row[project_description]</td>
                         <td>$row[CategoryName]</td>
                         <td>$row[subName]</td>
                         <td>
-                            <a class='btn btn-primary btn-sm' href='edit.php?id=$row[id]'>Edit</a>
-                            <a class='btn btn-danger btn-sm' href='delete.php?id=$row[id]'>Delete</a>
+                            <a class='btn btn-primary btn-sm' href='edit/editproject.php?id=$row[id_pro]'>Edit</a>
+                            <a class='btn btn-danger btn-sm' href='delete/deleteproject.php?id=$row[id_pro]'>Delete</a>
                         </td>
                     </tr>
                     ";
@@ -90,6 +83,9 @@ $active_categorie = '';
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
     <script src="dashboard.js"></script>
+    <script src="js/agents.js"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
 </body>
 
 </html>
