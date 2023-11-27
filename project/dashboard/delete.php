d0c8e3c5b662fe9c96bef2bf919e294e3680fa5e
lahcen <lahcen.ben3@gmail.com>
2023-11-27 16:47:58 +0100

    added delete user fonctionnality

--- project/dashboard/delete.php
+++ project/dashboard/delete.php
@@ -7,7 +7,7 @@ include('include/connect.php');
 // read the row of this id
 $sql = "DELETE FROM users WHERE ID_user = $id";
 $user = mysqli_query($con,$sql);
-header('location:agents.php');
+header('location:users.php');
 exit;
 }
 ?>
