f5ab6e0bef0015bba8f0cb2649b04b71cae8a9c7
lahcen <lahcen.ben3@gmail.com>
2023-11-28 21:22:55 +0100

    change login page

--- project/pages/login.php
+++ project/pages/login.php
@@ -1,61 +1,132 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>signup</title>
-      
-        <!-- style links -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/style.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-        <!-- animation links -->
-        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-        <!-- link for icons -->
-        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-      </head>
-<body>
 
-    <!-- Navbar -->
-    <?php include 'include/navbar.php' ?>
-    <section class="vh-100 bg-image"
-    style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp');">
-    <div class="mask d-flex align-items-center h-100 gradient-custom-3">
-      <div class="container h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-          <div class="col-12 col-md-9 col-lg-7 col-xl-6">
-            <div class="card" style="border-radius: 15px;">
-              <div class="card-body p-5">
-                <h2 class="text-uppercase text-center mb-5">login </h2>
-  
-                <form id="login-form">
-  
-                  <div class="form-outline mb-4">
-                    <label class="form-label" for="form3Example3cg">Your Email</label>
-                    <input type="text" id="login-mail_inp" class="form-control form-control-lg" />
-                  </div>
-  
-                  <div class="form-outline mb-4 ">
-                    <label class="form-label" for="form3Example4cg">Password</label>
-                    <input type="password" id="login-password_inp" class="form-control form-control-lg" />
-                    <div class="text-center mrgntop">
-                    <span id="login-mail_reg_err" class="text text-danger"></span>
-                    </div>
-                  </div>
-                  
-                  <button type="submit" class="mrgntop btn btn-primary primary-btn-orange">login</button>
-  
-                </form>
-  
-              </div>
-            </div>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="./css/login.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+  <div class="container">
+    <div class="forms-container">
+      <div class="signin-signup">
+        <!--the sign in form -->
+        <form  class="sign-in-form" method="post">
+
+          <h2 class="title">Sign in</h2>
+          <div class="input-field">
+            <i class="fas fa-user" style="color: #ff822e"></i>
+            <input type="text" placeholder="Username" />
+            <span class="msg-validation-signin">write a vailde Username</span>
           </div>
-        </div>
+          <div class="input-field">
+            <i class="fas fa-lock" style="color: #ff822e"></i>
+            <input type="password" placeholder="Password"/>
+            <span class="msg-validation-signin">write a vailde Password</span>
+
+          </div>
+          <input type="submit" value="Login" class="btn solid" />
+          <p class="social-text">Or Sign in with social platforms</p>
+          <div class="social-media">
+            <a href="#" class="social-icon">
+              <i class="fab fa-facebook-f"style="color: #ff822e"></i>
+            </a>
+            <a href="#" class="social-icon">
+              <i class="fab fa-twitter "style="color: #ff822e"></i>
+            </a>
+            <a href="#" class="social-icon">
+              <i class="fab fa-google" style="color: #ff822e"></i>
+            </a>
+            <a href="#" class="social-icon">
+              <i class="fab fa-linkedin-in" style="color: #ff822e"></i>
+            </a>
+          </div>
+        </form>
+        <!--the Sign up form -->
+        <form  class="sign-up-form" method="post">
+
+          <h2 class="title">Sign up</h2>
+          <div class="input-field">
+            <i class="fas fa-user" style="color: #ff822e"></i>
+            <input type="text" name="username" placeholder="Username" />
+            <span class="msg-validation-signin">write a vailde Username</span>
+          </div>
+
+          <div class="input-field">
+            <i class="fas fa-envelope" style="color: #ff822e"></i>
+            <input type="email" name="email" placeholder="Email" />
+            <span class="msg-validation-signin">write a vailde Email</span>
+          </div>
+
+          <div class="input-field">
+            <i class="fas fa-envelope" style="color: #ff822e"></i>
+            <input type="date" name="birtday" placeholder="Email" />
+            <span class="msg-validation-signin">write a vailde Email</span>
+          </div>
+
+          <div class="input-field">
+            <i class="fas fa-lock" style="color: #ff822e"></i>
+            <input type="password" name="password" placeholder="Password" />
+            <span class="msg-validation-signin">write a vailde Username</span>
+          </div>
+
+          <input type="submit" class="btn" value="Sign up" />
+
+          <p class="social-text">Or Sign up with social platforms</p>
+          <div class="social-media">
+            <a href="#" class="social-icon">
+              <i class="fab fa-facebook-f" style="color: #ff822e"></i>
+            </a>
+            <a href="#" class="social-icon">
+              <i class="fab fa-twitter" style="color: #ff822e"></i>
+            </a>
+            <a href="#" class="social-icon">
+              <i class="fab fa-google" style="color: #ff822e"></i>
+            </a>
+            <a href="#" class="social-icon">
+              <i class="fab fa-linkedin-in" style="color: #ff822e"></i>
+            </a>
+          </div>
+        </form>
       </div>
     </div>
-  </section>
+    <div class="panels-container">
+      <div class="panel left-panel">
+        <div class="content">
+          <h3>New here ?</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
+            ex ratione. Aliquid!
+          </p>
+          <button class="btn transparent" id="sign-up-btn">
+            Sign up
+          </button>
+        </div>
+        <img src="images/signup.svg" class="image" alt="" />
+      </div>
+      <div class="panel right-panel">
+        <div class="content">
+          <h3>One of us ?</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            laboriosam ad deleniti.
+          </p>
+          <button class="btn transparent" id="sign-in-btn">
+            Sign in
+          </button>
+        </div>
+        <img src="images/signup.svg" class="image" alt="" />
+      </div>
+    </div>
+  </div>
 
-  <?php include 'include/footer.php'?>
-  <script src="javascript/valid_login.js"></script>
+  <script src="./javascript/login.js"></script>
+
 </body>
+
 </html>
