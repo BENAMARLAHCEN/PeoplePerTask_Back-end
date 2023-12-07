49411413d3e9d380bc8162a6a36e02ab2f1bd33c
lahcen <lahcen.ben3@gmail.com>
2023-12-07 09:00:31 +0100

    create send proposal functionnality

---
+++ project/profile/proposal.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'controller/sessionController.php';
+require_once '../dashboard/include/connect.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<?php require_once 'include/head.php'  ?>
+
+<body>
+
+    <div class="container-xl px-4 mt-4">
+
+        <?php require_once 'include/nav.php'  ?>
+        
+
+        
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+
+    </script>
+</body>
+
+</html>
